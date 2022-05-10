c21dd459369365ee9cfc1d36552a5698d8a66012
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-05-10 22:20:03 +0600

    new code added

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -6,10 +6,25 @@ import phone from "../../assets/icons/phone.svg";
 
 const Info = () => {
   return (
-    <div className="px-0 md:px-5 lg:px-20 grid grid-col-1 md:grid-col-2 lg:grid-cols-3 gap-5 mx-auto ">
-      <InfoCard img={clock}></InfoCard>
-      <InfoCard img={marker}></InfoCard>
-      <InfoCard img={phone}></InfoCard>
+    <div className=" md:px-5 lg:px-20 grid grid-col-1 md:grid-col-2 lg:grid-cols-3 gap-5 mx-auto pb-10">
+      <InfoCard
+        cardTitle="Opening Hours"
+        cardText="Lorem Ipsum is simply dummy text of the pri"
+        img={clock}
+        bgClass="bg-primary"
+      ></InfoCard>
+      <InfoCard
+        cardTitle="Visit our location"
+        cardText="Brooklyn, NY 10036, United States"
+        img={marker}
+        bgClass="bg-accent"
+      ></InfoCard>
+      <InfoCard
+        cardTitle="Contact us now"
+        cardText="+000 123 456789"
+        img={phone}
+        bgClass="bg-primary"
+      ></InfoCard>
     </div>
   );
 };
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-const InfoCard = (props) => {
+const InfoCard = ({ img, cardTitle, bgClass }) => {
   return (
     <div>
-      <div class="card lg:card-side bg-primary shadow-xl text-white px-0 md:px-5 lg:px-10">
-        <figure>
-          <img src={props.img} alt="Album" />
+      <div
+        class={`card lg:card-side ${bgClass} shadow-xl text-white px-0 md:px-5 lg:px-10 py-5 md:py-0`}
+      >
+        <figure className="pl-5">
+          <img className="text-white" src={img} alt="Album" />
         </figure>
         <div class="card-body">
-          <h2 class="card-title">New album is released!</h2>
+          <h2 class="card-title">{cardTitle}</h2>
           <p>Click the button to listen on Spotiwhy app.</p>
         </div>
       </div>
