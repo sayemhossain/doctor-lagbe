a2039d396a3a947427957ce5b4f18757e8766b79
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-05-10 20:08:13 +0600

    new code added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
