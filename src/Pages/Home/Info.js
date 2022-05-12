de388431a7367dc1249142583a42d3c47a87c0d8
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-05-13 03:38:47 +0600

    home is almost done with out full responsive

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -6,24 +6,24 @@ import phone from "../../assets/icons/phone.svg";
 
 const Info = () => {
   return (
-    <div className="px-5 md:px-5 lg:px-10 grid grid-col-1 md:grid-col-2 lg:grid-cols-3 gap-5 pb-10">
+    <div className="px-5 md:px-5 lg:px-10 grid grid-col-1 md:grid-col-2 lg:grid-cols-3 gap-5 pb-5">
       <InfoCard
         cardTitle="Opening Hours"
         cardText="Lorem Ipsum is simply dummy text of the pri"
         img={clock}
-        bgclassName="bg-primary bg-gradient-to-r from-cyan-500 to-blue-500"
+        bgClass="bg-primary bg-gradient-to-r from-cyan-500 to-blue-500"
       ></InfoCard>
       <InfoCard
         cardTitle="Visit our location"
         cardText="Brooklyn, NY 10036, United States"
         img={marker}
-        bgclassName="bg-accent"
+        bgClass="bg-accent"
       ></InfoCard>
       <InfoCard
         cardTitle="Contact us now"
         cardText="+000 123 456789"
         img={phone}
-        bgclassName="bg-primary bg-gradient-to-r from-cyan-500 to-blue-500"
+        bgClass="bg-primary bg-gradient-to-r from-cyan-500 to-blue-500"
       ></InfoCard>
     </div>
   );
