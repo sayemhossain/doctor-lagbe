8c311356ea06e9cb05cb3f05333e9bd8bfaeaee9
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-05-13 22:08:09 +0600

    new code added

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,14 +1,19 @@
 import React from "react";
 
 const BookingModal = ({ treatment }) => {
+  const { name, solts } = treatment;
   return (
     <div>
       <input type="checkbox" id="booking-modal" class="modal-toggle" />
       <div class="modal modal-bottom sm:modal-middle">
         <div class="modal-box">
-          <h3 class="font-bold text-lg">
-            Congratulations random Interner user!
-          </h3>
+          <label
+            for="booking-modal"
+            class="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 class="font-bold text-lg">{name}</h3>
           <p class="py-4">
             You've been selected for a chance to get one year of subscription to
             use Wikipedia for free!
