dae371e2d0aab162b35fcc1006c9cec41bcad7c1
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-05-14 17:48:56 +0600

    login form added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <h3>This is login</h3>
+    <div className="flex justify-center items-center h-screen">
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title mx-auto">Login</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
