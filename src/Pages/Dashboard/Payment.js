a064186943713a0527aed1b5a6c115199447ca60
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-05-21 19:04:57 +0600

    payment method implementing

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,7 +1,14 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import Loading from "../Shared/Loading";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(
+  "pk_test_51L1rB9J1SvemP9KBSWYsv07hEcXus8uWOEaw7YkkJkmVq6rDOkO7AjP65OrfrAzODqWPHRoPOQrBv4BTlFiorVnv00rNDhJrJr"
+);
 
 const Payment = () => {
   const { id } = useParams();
@@ -25,18 +32,21 @@ const Payment = () => {
           <div class="hero-content flex-col">
             <div class="card w-50 max-w-lg bg-base-100 shadow-xl">
               <div class="card-body">
-                <p className="text-center">
-                  <small>Hello, {appointment.patientName}</small>
+                <p className="">
+                  <small className="text-primary ">
+                    Hello, {appointment.patientName}
+                  </small>
                 </p>
-                <h2 class=" md:text-xl font-bold text-center">
-                  Please pay for {appointment.treatment}.
+                <h2 class="uppercase mb-5">
+                  Please pay for{" "}
+                  <span className="font-bold">{appointment.treatment}.</span>
                 </h2>
-                <p className="text-sm text-center">
+                <p className="text-sm">
                   Your appointment on{" "}
                   <span className="text-red-400">{appointment.date} </span> at{" "}
                   <span>{appointment.slot}</span>
                 </p>
-                <p className="text-center">
+                <p>
                   Amount:{" "}
                   <span className="text-red-400 font-bold">
                     ${appointment.price}
@@ -45,7 +55,16 @@ const Payment = () => {
               </div>
             </div>
             <div class="card flex-shrink-0 w-50 max-w-lg shadow-2xl bg-base-100">
-              <div class="card-body"></div>
+              <div class="card-body">
+                <Elements stripe={stripePromise}>
+                  <CheckoutForm />
+                </Elements>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+                  delectus laboriosam velit veritatis. Aspernatur repellat
+                  consequuntur porro eius fugiat minima!
+                </p>
+              </div>
             </div>
           </div>
         </div>
